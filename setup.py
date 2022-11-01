d8289e855c30922f855bc8ba6d8c0f4cb8f15c24
Ryo <try0.development@gmail.com>
2022-11-01 21:25:23 +0900

    0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 # twine upload --repository pypi dist/*
 setup(
     name='mkdocs-toc-md',
-    version='0.0.2',
+    version='0.0.3',
     description='Generate a toc markdown file',
     long_description=io.open('README.md', encoding='utf8').read(),
     long_description_content_type='text/markdown',
