5011d9be27306f9b4bb2327209938619f79a3df5
Ryo <try0.development@gmail.com>
2023-05-28 14:42:58 +0900

    fix import

--- mkdocs_toc_md/hook.py
+++ mkdocs_toc_md/hook.py
@@ -12,7 +12,7 @@ from mkdocs.config.base import Config
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from mkdocs_toc_md.plugin import TocItem
+    from mkdocs_toc_md.objects import TocItem
 
 
 
